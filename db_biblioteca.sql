9e4b14f4f1455172f06a4dc6dde71c978800787c
eduardaperez <97349220+eduardaperez@users.noreply.github.com>
2023-10-10 23:29:33 -0300

    Finalização com teste de DB conforme solicitado

--- db_biblioteca.sql
+++ db_biblioteca.sql
@@ -1,11 +1,15 @@
 /*DROP DATABASE IF EXISTS Biblioteca;*/
 
+/*1) Criar a base de dados e prepará-la para uso. */
 CREATE DATABASE Biblioteca
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_0900_ai_ci;
 
 USE Biblioteca;
 
+/*2) Criar as tabelas de acordo com o modelo lógico relacional idealizado na avaliação anterior, considerando
+as restrições de integridade; a base de dados deve conter pelo menos cinco tabelas, análogas a: livro,
+autor, membro, categoria, empréstimo. */
 CREATE TABLE Autor(
 	id_autor INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nome_autor VARCHAR (30) NOT NULL,
@@ -64,7 +68,7 @@ CREATE TABLE LivrosEmprestados (
     FOREIGN KEY (isbn) REFERENCES Livros(isbn)
 );
 
-
+/*3) Inserir pelo menos 5 registros em cada tabela. */
 INSERT INTO Autor (nome_autor, nacionalidade)
 VALUES
     ('George Orwell', 'Inglaterra'),
@@ -93,13 +97,14 @@ MODIFY COLUMN ano_publicacao INT;
 INSERT INTO Livros (isbn, titulo, ano_publicacao, editora, qtd_disponivel, fk_id_autor, fk_id_categoria)
 VALUES
     ('9780451524935', '1984', 1949, 'Penguin', 10, 1, 1),
-    ('9780061120084', 'Cem Anos de Solidão', 1967, 'Harper & Row', 8, 2, 2),
+    ('9780061120084', 'Cem Anos de Solidão', 1967, 'Harper & Row', 3, 2, 2),
     ('9780141439723', 'Orgulho e Preconceito', 1813, 'Penguin', 12, 3, 3),
-    ('9780140620590', 'As Aventuras de Huckleberry Finn', 1884, 'Penguin', 7, 4, 4),
+    ('9780140620590', 'As Aventuras de Huckleberry Finn', 1884, 'Penguin', 5, 4, 4),
     ('9780439554930', 'Harry Potter e a Pedra Filosofal', 1997, 'Bloomsbury', 15, 1, 1),
     ('9780451169525', 'O Iluminado', 1977, 'Signet', 10, 2, 2),
     ('9780062073475', 'Assassinato no Expresso Oriente', 1934, 'HarperCollins', 12, 3, 3),
     ('9780385514249', 'Uma Breve História do Tempo', 1988, 'Bantam', 8, 4, 4);
+    
 
 INSERT INTO Membros (nome, cpf, data_nascimento, email, telefone, endereco)
 VALUES
@@ -116,8 +121,7 @@ VALUES
     (2),
     (3),
     (4),
-    (5),
-    (6);
+    (5);
     
 INSERT INTO LivrosEmprestados (id_emprestimo, isbn)
 VALUES
@@ -128,8 +132,58 @@ VALUES
     (4, 9780439554930),
     (4, 9780451169525),
     (5, 9780062073475),
-    (6, 9780141439723);
+    (5, 9780141439723);
     
-SELECT *
-FROM LivrosEmprestados;
+/*4) Listar todos os autores cujo nome inicia com a letra ‘A’. */
+SELECT nome_autor
+FROM Autor
+WHERE nome_autor LIKE 'a%';
 
+/*5) Listar todos os livros cujo título contenha a palavra ‘sistema’. */
+SELECT titulo
+FROM Livros
+WHERE titulo LIKE '%sistema%';
+
+/*6) Listar a chave primária e o título dos livros que foram publicados há mais de 5 anos. */
+SELECT isbn, titulo, ano_publicacao
+FROM Livros
+WHERE ano_publicacao < 2023 - 5
+ORDER BY ano_publicacao;
+
+/*7) Listar, em ordem alfabética por título, os livros que possuem menos de 5 exemplares disponíveis. */
+SELECT titulo, qtd_disponivel
+FROM Livros
+WHERE qtd_disponivel < 5
+ORDER BY titulo;
+
+/*8) Listar todos os livros que nunca foram emprestados. */
+SELECT Livros.isbn, Livros.titulo AS 'Nunca emprestado'
+FROM Livros
+LEFT JOIN LivrosEmprestados ON Livros.isbn = LivrosEmprestados.isbn
+WHERE LivrosEmprestados.isbn IS NULL;
+
+/*9) Mudar a data de devolução real de todos os empréstimos para a data atual. */
+UPDATE Emprestimos
+SET data_devolvido = CURRENT_DATE
+WHERE data_devolvido IS NULL;
+
+SELECT *
+FROM Emprestimos;
+
+/*10) Postergar em um mês a data de devolução prevista de todos os empréstimos. */
+UPDATE Emprestimos
+SET data_prevista_devolucao = DATE_ADD(data_prevista_devolucao, INTERVAL 30 DAY);
+
+/*11) Excluir todos os membros que nunca fizeram empréstimo. */
+DELETE FROM Membros
+WHERE id_membro NOT IN (SELECT DISTINCT id_membro FROM Emprestimos);
+
+SELECT *
+FROM Membros;
+
+/*12) Excluir todas as categorias que não aparecem em nenhum livro. */
+DELETE FROM Categorias
+WHERE id_categoria NOT IN (SELECT DISTINCT fk_id_categoria FROM Livros);
+
+SELECT *
+FROM Categorias;
